54c8adb3504397fdbbc67f3094a6dfe05a26dd9d
OKozmovskaia <olga.kozmovskaia@gmail.com>
2020-10-12 10:41:10 +0200

    Complete 1.0

---
+++ 01-Introduction/1.0.js
@@ -0,0 +1,14 @@
+const readlineSync = require("readline-sync");
+
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);
+
+console.log('10' + '5');
+
+console.log(10 + 5);
+
+console.log(10 * 5);
+
+console.log(10 / 5);
+
+console.log(10 % 5);

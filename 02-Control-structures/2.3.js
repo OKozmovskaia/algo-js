85f0355ca4740ceab5fb227fefe459b19e9cb1ac
OKozmovskaia <olga.kozmovskaia@gmail.com>
2020-10-12 15:28:39 +0200

    Complete 2.3

---
+++ 02-Control-structures/2.3.js
@@ -0,0 +1,21 @@
+let i = 1
+
+while (i <= 100) {
+  
+  if ((i % 2) === 0) {
+    console.log(i);
+    i += 1;
+  } else {
+    i += 1;
+  }
+
+}
+
+
+for (let i = 1; i <= 100; i += 1) {
+
+  if ((i % 2) === 0) {
+    console.log(i);
+  }
+
+}

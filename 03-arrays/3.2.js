beaa3d6645dc0a4cab019a981ac4d11cd0f14865
OKozmovskaia <olga.kozmovskaia@gmail.com>
2020-10-13 09:15:11 +0200

    Complete 3.2

---
+++ 03-arrays/3.2.js
@@ -0,0 +1,13 @@
+let arr = [];
+let result = 0;
+
+// arr = [1,2,3,4,5];
+arr = [100,101,102];
+
+for (let elem of arr) {
+  result += elem; 
+}
+
+let average = result / arr.length;
+
+console.log(average);

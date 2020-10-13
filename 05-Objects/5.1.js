a48753ed7d4131ddf0eb2a28fc5798f52955bedb
OKozmovskaia <olga.kozmovskaia@gmail.com>
2020-10-13 15:53:00 +0200

    Complete 5.1

---
+++ 05-Objects/5.1.js
@@ -0,0 +1,47 @@
+const readlineSync = require("readline-sync");
+
+askTvSerie();
+
+
+function askTvSerie() {
+
+// the main object
+  let objectSerial = {
+    name: " ",
+    productionYear: " ",
+    namesActors: []
+  }
+
+  let nameSerial = readlineSync.question("What your favorite serial? ");
+  objectSerial.name = nameSerial;
+
+  let yearSerial = readlineSync.question("What year did it come? ");
+  objectSerial.productionYear = yearSerial;
+
+  let numberOfActors = new Number(readlineSync.question("How much actors your remember on that serial? "));
+  // the nested object
+  for (let i = 1; i <= numberOfActors; i++) {
+    console.log(numberOfActors);
+    let actor = {
+      name: " ",
+      role: " "
+    }
+
+    let actorSerial = readlineSync.question("Name, please: ");
+    actor.name = actorSerial;
+
+    let roleActor = readlineSync.question("The role of him: ");
+    actor.role = roleActor;
+
+    objectSerial.namesActors.push(actor);
+  }
+
+  console.log(objectSerial);
+}
+
+
+
+
+
+
+

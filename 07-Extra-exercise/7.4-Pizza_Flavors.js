86c58e66486f7a95f42dd005ba4be4b08f88fad2
OKozmovskaia <olga.kozmovskaia@gmail.com>
2020-10-20 10:39:19 +0200

    Fix bug with loop comands, add function for save data in local storage, but it doesn't work, because we haven't index.html and haven't local storage

--- 07-Extra-exercise/7.4-Pizza_Flavors.js
+++ 07-Extra-exercise/7.4-Pizza_Flavors.js
@@ -19,30 +19,44 @@ checkNumber(number);
 // check the number of menu
 function checkNumber(n) {
 
-  while(n === 4) {
-
-    if (n === 1) {
-      // display the current list of flavor
-      console.log("The list of flavors for pizza: " + arr);
-      
+  // validate the command
+  if(n === 1) {
+    // display the current list of flavor
+    console.log("The list of flavors for pizza: " + arr);
+    localStorage.getItem(arr);
     
-    } else if (n === 2) {
-      // add new pizza flavor
-      let newPizzaFlavor = readlineSync.question("Add your flavor? ");
-      arr.push(newPizzaFlavor);
+  } else if (n === 2) {
+    // add new pizza flavor
+    let newFlavor = readlineSync.question("Add your flavor? ");
+    addNewFlavor(newFlavor);
     
-    } else if (n == 3) {
+    } else if (n === 3) {
       // remove flavor from list
-      let numberOfPizzaFlavor = new Number(readlineSync.question("Add number of flavor that you want to delete? "));
+      let numberOfPizzaFlavor = new Number(readlineSync.question("Add number of flavor what you want to delete? "));
       arr.forEach(() => {
         if (numberOfPizzaFlavor === arr[x]) {
           arr.splice(x, 1);
         }
-      })
-    } 
-  }
-  takeNumber();
+      })} else if(n === 4) {
+        return arr;
+
+        // validate another numbers
+      } else {
+        takeNumber();
+        checkNumber(number);
+      }
 }
+
+console.log(arr);
+
+function addNewFlavor(flavor) {
+  arr = JSON.parse(localStorage.getItem('arr')) || [];
+  arr.push(flavor);
+  localStorage.setItem('arr', JSON.stringify(arr));
+
+  let newPizzaFlavor = readlineSync.question("Add your flavor? ");
+  addNewFlavor(newPizzaFlavor);
+  }
 
 
 
